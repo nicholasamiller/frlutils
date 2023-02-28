87762a2a12c0ce8c983333aff1e4101eff2153d6
Nicholas Andrew Miller <nick.miller@govlawtech.com.au>
2023-02-28 13:59:55 +1100

    version bump

--- nugetPackAndPush.ps1
+++ nugetPackAndPush.ps1
@@ -1,3 +1,3 @@
 dotnet pack
 
-dotnet nuget push ((gci '.\bin\Debug\*.nupkg') | select -First 1).FullName --api-key $nugetKey --source https://api.nuget.org/v3/index.json
+dotnet nuget push (((gci '.\bin\Debug\*.nupkg') | Sort-Object LastWriteTime -Descending | select -First 1).FullName) --api-key $nugetKey --source https://api.nuget.org/v3/index.json
